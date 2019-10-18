56a8ec28fb65ff699b3557d83824f9d02ea58564
Halil Özaktaş <h.ozaktas@gmail.com>
2019-10-18 14:38:22 +0300

    first release

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 )
 
 var (
-	ip         = flag.String("ip", "127.0.0.1", "The host of redis")
-	port       = flag.Uint("port", 6379, "The port of redis")
+	host       = flag.String("host", "127.0.0.1", "The host of redis")
+	port       = flag.String("port", "6379", "The port of redis")
 	password   = flag.String("password", "", "The password of redis")
 	reportPath = flag.String("reportPath", "./reports", "The csv file path of analysis result")
 )
@@ -18,22 +18,18 @@ func main() {
 
 	flag.Parse()
 
-	analysis, err := NewAnalysisConnection(*ip, uint16(*port), *password)
-	if err != nil {
-		fmt.Println("something wrong:", err)
-		return
-	}
-	defer analysis.Close()
+	Connection(*host, *port, *password)
+	Start([]string{":", "_"})
 
-	analysis.Start([]string{":"})
-
-	//Find the csv file in default target folder: ./reports
-	//CSV file name format: redis-analysis-{host:port}-{db}.csv
-	//The keys order by count desc
-	err = analysis.SaveReports(*reportPath)
+	// //Find the csv file in default target folder: ./reports
+	// //CSV file name format: redis-analysis-{host:port}-{db}.csv
+	// //The keys order by count desc
+	err := SaveReports(*reportPath)
 	if err == nil {
 		fmt.Println("\ndone")
 	} else {
 		fmt.Println("error:", err)
 	}
+
+	Close()
 }
